9c34249b9bc35ee0c338feda39b4f42d72fa9ce9
Alli Tzadok <allisontzadok@gmail.com>
2018-04-30 07:42:32 -0500

    add generic CRUD methods to calcendar controller as a foundation to build off

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,2 +1,47 @@
 class CalendarsController < ApplicationController
+
+  before_action :set_calendar, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @calendars = Calendar.all
+  end
+
+  def new
+    @calendar = Calendar.new
+  end
+
+  def edit
+  end
+
+  def create
+    @calendar = Calendar.new(calendar_params)
+    if @calendar.save
+      redirect_to calendar_path(@calendar)
+    else
+      redirect_to new_calendar_path
+    end
+  end
+
+  def update
+    if @calendar.update(calendar_params)
+      redirect_to calendar_path(@calendar)
+    else
+      redirect_to edit_calendar_path(@calendar)
+    end
+  end
+
+  def destroy
+    @calendar.destroy
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def set_calendar
+    @calendar = Calendar.find(params[:id])
+  end
+
+  def calendar_params
+    params.require(:calendar).permit(:name)
+  end 
 end
