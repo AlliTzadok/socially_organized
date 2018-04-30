3f80de7ecdcff6d4816164f4450bc5f37be7703a
Alli Tzadok <allisontzadok@gmail.com>
2018-04-30 07:49:20 -0500

    add basic index, show and new/create methods to platforms controller. No permissions to ever edit, update or destroy these

--- app/controllers/platforms_controller.rb
+++ app/controllers/platforms_controller.rb
@@ -1,2 +1,33 @@
 class PlatformsController < ApplicationController
+  before_action :set_platform, only: [:show]
+
+  def index
+    @platforms = Platform.all
+  end
+
+  def show
+  end
+
+  def new
+    @platform = Platform.new
+  end
+
+  def create
+    @platform = Platform.new(platform_params)
+    if @platform.save
+      redirect_to dashboard_path
+    else
+      redirect_to new_platform_path
+    end
+  end
+
+  private
+
+  def set_platform
+    @platform = Platform.find(params[:id])
+  end
+
+  def platform_params
+    params.require(:platform).permit(:name)
+  end 
 end
