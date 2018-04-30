58683be5a43f4bfd16c014f995cb8c0bab3506a5
Alli Tzadok <allisontzadok@gmail.com>
2018-04-30 11:19:49 -0500

    add associations to calendar model

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,2 +1,20 @@
 class Calendar < ApplicationRecord
+
+  has_many :user_calendars
+  has_many :users, through: :user_calendars
+  has_many :calendar_posts
+  has_many :posts, through: :calendar_posts
+
+  #need method to find all calendar posts for today.
+#does it need to be calendar.posts.where(date: Date.current)? or some other way
+def todays_calendar
+  current_user.calendars.each do |calendar|
+    calendar.posts.where(date: Date.current)
+  end
+end
+
+#need method to query and verify current user is a user on the calendar
+def authorized_for_calendar
+
+end
 end
