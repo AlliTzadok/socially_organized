d5d25ce21e7158feb911cd7aac000b726dcfce3a
Alli Tzadok <allisontzadok@gmail.com>
2018-04-30 11:22:38 -0500

    add associations to post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  has_many :calendar_posts
+  has_many :calendars, through: :calendar_posts
+  has_many :platform_posts
+  has_many :platforms, through: :platform_posts
+  belongs_to :user
+  
   mount_uploader :picture, PictureUploader
 end
