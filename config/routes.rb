26c1e376af458ddd2a4683f8de72dee410008f56
Alli Tzadok <allisontzadok@gmail.com>
2018-04-30 11:39:45 -0500

    add user persisted functionality to user/omniauth controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,11 @@
 Rails.application.routes.draw do
-  authenticated :user do
-    root 'welcome#home', as 'authenticated_root'
-  end
-
-  devise_scope :user do
-    root 'devise/sessions#new'
-  end
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  get '/auth/facebook/callback' => 'sessions#create'
+  root 'welcome#home'
 
   resources :posts
   resources :platforms
   resources :calendars
 
-  root 'welcome#home'
+
 end
