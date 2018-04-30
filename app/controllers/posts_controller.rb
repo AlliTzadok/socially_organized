4f7e00b5d0674f933ae5ca0cfbe7259e87682356
Alli Tzadok <allisontzadok@gmail.com>
2018-04-30 12:03:33 -0500

    update methods in post controller for current user associations and add notices

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,21 +2,22 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
-    @posts = Post.all
+    @posts = current_user.posts
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
-    @post = Post.new
+    @post = current_user.posts.build
   end
 
   def edit
   end
 
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to post_path(@post)
     else
@@ -26,7 +27,7 @@ class PostsController < ApplicationController
 
   def update
     if @post.update(post_params)
-      redirect_to post_path(@post)
+      redirect_to post_path(@post), notice: 'Your post has been updated.'
     else
       redirect_to edit_post_path(@post)
     end
@@ -34,7 +35,7 @@ class PostsController < ApplicationController
 
   def destroy
     @post.destroy
-    redirect_to posts_path
+    redirect_to posts_path, notice: 'Your post has been deleted.'
   end
 
   private
@@ -43,6 +44,6 @@ class PostsController < ApplicationController
   end
 
   def post_params
-    params.require(:post).permit(:title, :content, :link, :finalized, :image_link, :user_id)
-  end 
+    params.require(:post).permit(:title, :content, :link, :finalized, :picture, :user_id)
+  end
 end
