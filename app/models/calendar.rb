f3dfd7505ce5dbc682205bc95053344eb46d5703
Alli Tzadok <allisontzadok@gmail.com>
2018-05-01 17:02:14 -0500

    try to fix issue with filtering out finalized vs not calendar post plans

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -6,22 +6,21 @@ class Calendar < ApplicationRecord
   has_many :posts, through: :calendar_posts
 
   #need method to find all calendar posts for today.
-#does it need to be calendar.posts.where(date: Date.current)? or some other way
-def todays_calendar
-  current_user.calendars.each do |calendar|
-    calendar.posts.where(date: Date.current)
+  #does it need to be calendar.posts.where(date: Date.current)? or some other way
+  def todays_calendar
+    current_user.calendars.each do |calendar|
+      calendar.posts.where(date: Date.current)
+    end
   end
-end
 
-def rough_draft_posts
-  self.posts.each do |post|
+  def rough_draft_posts
+    self.posts.each do |post|
       post.where(finalized: false)
     end
   end
-end
 
-#need method to query and verify current user is a user on the calendar
-def authorized_for_calendar
+  #need method to query and verify current user is a user on the calendar
+  def authorized_for_calendar
 
-end
+  end
 end
