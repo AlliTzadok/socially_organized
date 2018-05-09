21873eaeb5e654a6d656e8194ff99342c0527ddf
Alli Tzadok <allisontzadok@gmail.com>
2018-05-09 10:05:44 -0500

    remove 2nd users file in the controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
+  def github
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to root_path
     end
-  end
-
-  def failure
-    redirect_to root_path    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.info.name  #assuming the user model has a name
       user.oauth_token = auth.credentials.token
-      user.image = auth.info.image #assuming the user model has an image
+      # user.image = auth.info.image #assuming the user model has an image
       user.save!
     end
   end
