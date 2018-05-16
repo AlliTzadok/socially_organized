b404ce5ffb96c42a2c21336022f51ce569e815d3
Alli Tzadok <allisontzadok@gmail.com>
2018-05-15 21:46:18 -0500

    remove code that is not being used

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def dashboard
-    @user = current_user
-    @calendars = current_user.calendars
+
   end
 
 end
--- app/models/calendar_post.rb
+++ app/models/calendar_post.rb
@@ -1,7 +1,6 @@
 class CalendarPost < ApplicationRecord
   belongs_to :calendar
   belongs_to :post
-  scope :post_today, -> { where(date: Date.current)}
   validates :post_id, uniqueness: { scope: :calendar_id}
 
 
